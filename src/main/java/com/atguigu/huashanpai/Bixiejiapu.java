403a002935135ea02181a0fd9a74bf78e8c83af3
shanzhongheshi <869060393@qq.com>
2020-09-08 07:16:00 +0800

    idea-second

---
+++ src/main/java/com/atguigu/huashanpai/Bixiejiapu.java
@@ -0,0 +1,11 @@
+package com.atguigu.huashanpai;
+
+/**
+ * @author Lin
+ * 2020/9/8  7:14
+ */
+public class Bixiejiapu {
+    public static void main(String[] args) {
+        System.out.println("这本秘籍有bug");
+    }
+}
