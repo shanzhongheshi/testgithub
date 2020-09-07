0f992197c383512927a776c4cb3c863e4f57b5aa
shanzhongheshi <869060393@qq.com>
2020-09-08 07:08:01 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/huashanpai/Dugujianfa.java
@@ -0,0 +1,11 @@
+package com.atguigu.huashanpai;
+
+/**
+ * @author Lin
+ * 2020/9/8  7:06
+ */
+public class Dugujianfa {
+    public static void main(String[] args) {
+        System.out.println("独孤剑法真牛");
+    }
+}
